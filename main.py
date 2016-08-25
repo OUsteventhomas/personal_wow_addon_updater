75fbda9178699022946d946b6a063915a36c16a2
OUsteventhomas <steven.thomas@steven-DT.hq.local>
2016-08-25 10:58:29 -0500

    Initial Commit
    Project creation commit

---
+++ main.py
@@ -0,0 +1,104 @@
+import requests
+import re
+import sqlite3
+import zipfile as zip
+import os
+import wget
+
+
+class addonUpdater():
+
+    dloads_path = r"C:\example\dloads"
+    wow_addon_directory = r"F:\World of Warcraft\Interface\AddOns\\"
+    os.chdir(dloads_path)
+    path = os.getcwd()
+    db_file = "{0}\\addons.db".format(os.path.dirname(os.getcwd()))
+    wow_path = wow_addon_directory[:-1]
+
+    def main(self):
+        self.db_connect()
+        self.db_select_all_data()
+        self.select_out_of_date()
+        self.unzip_addons()
+        self.set_all_versions_current()
+        self.db_close_connection()
+
+    def db_connect(self):
+        self.conn = sqlite3.connect(self.db_file)
+
+    def db_select_all_data(self):
+        cursor = self.conn.execute("SELECT * FROM info")
+        for row in cursor:
+            self.name = row[0]
+            self.url_partial = row[1]
+            self.installed_version = row[2]
+            self.cur_version = row[3]
+            self.needs_updating = row[4]
+            self.version_update()
+            self.addon_update_db()
+
+    def set_all_versions_current(self):
+        update_query = "UPDATE info SET v_installed = v_available"
+        self.conn.execute(update_query)
+        second_update_query = "UPDATE info SET up_to_date = '1'"
+        self.conn.execute(second_update_query)
+        self.commit_db_changes()
+
+    def select_out_of_date(self):
+        cursor = self.conn.execute("SELECT * FROM info WHERE up_to_date = '0'")
+        for row in cursor:
+            self.url_partial = row[1]
+            self.download_zip_file_page()
+
+    def download_zip_file_page(self):
+        url = "https://mods.curse.com/addons/wow/{0}/download".format(self.url_partial)
+        counter = 1
+        r = requests.get(url)
+        page_text = r.text
+        link = re.findall("http://.*\.zip", page_text)[0]
+        wget.download(link)
+
+    def quit_all(self):
+        sys.exit()
+
+    def version_update(self):
+        url = "https://mods.curse.com/addons/wow/{0}".format(self.url_partial)
+        r = requests.get(url)
+        data = r.text
+        self.newest_version = re.sub("Newest File: ", "", re.findall("Newest File:.*\<", data)[0][:-1])
+        if self.newest_version != self.cur_version:
+            self.db_version_update()
+
+    def db_version_update(self):
+        update_query = "UPDATE info SET v_available = '{0}' WHERE url_partial = '{1}'".format(self.newest_version, self.url_partial)
+        self.conn.execute(update_query)
+        self.commit_db_changes
+
+    def addon_update_db(self):
+        if self.installed_version == self.cur_version and self.needs_updating != "1":
+            up_to_date_query = "UPDATE info SET up_to_date = '1' WHERE name = '{0}'".format(self.name)
+            self.conn.execute(up_to_date_query)
+        elif self.installed_version != self.cur_version and self.needs_updating != "0":
+            not_up = "UPDATE info SET up_to_date = '0' WHERE name = '{0}'".format(self.name)
+            self.conn.execute(not_up)
+        self.commit_db_changes()
+
+    def unzip_addons(self):
+        for dirName, subdirlist, filelist in os.walk(self.path):
+            for fname in filelist:
+                if fname.endswith(".zip") is True:
+                    src_path = "\\".join((dirName, fname))
+                    zipcheck = zip.is_zipfile(src_path)
+                    if zipcheck is True:
+                        with zip.ZipFile(src_path) as zf:
+                            zf.extractall(self.wow_path)
+                        os.remove(src_path)
+
+    def commit_db_changes(self):
+        self.conn.commit()
+
+    def db_close_connection(self):
+        self.conn.close()
+
+if __name__ == "__main__":
+    addonUpdater().main()
---
+++ readme.txt
@@ -0,0 +1,19 @@
+This simple python script written in python 3.5 will update your wow addons
+to the latest version available on curse on a Windows machine.
+
+-----Instructions-----
+
+1) Place main.py and addons.db in the same folder
+2) Create dloads directory in same folder where main.py and addons.db exist
+3) Update the sqlite database to have the addons you wish to have installed.
+    a. Column "name" = An easy name for you to remember
+    b. Column "url_partial" = The last part of the URL when on that addon's page
+        eg: Deadly Boss Mods curse page is "https://mods.curse.com/addons/wow/deadly-boss-mods"
+        so the entry for url_partial on Deadly Boss Mods is "deadly-boss-mods"
+    c. Columns v_installed, v_available and up_to_date can be left empty
+4) Set two variables in this script before running
+    a. dloads_path
+    b. wow_addon_directory
+
+5) Run script and play WoW
+--------------------
